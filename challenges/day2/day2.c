0e6b22f7b53e7de39c17773aea967ec53b8a926e
r3v <yada@yada.com>
2024-12-12 21:45:28 -0700

    i feel dumb

--- challenges/day2/day2.c
+++ challenges/day2/day2.c
@@ -5,70 +5,72 @@
 #define ARR_SIZE 256
 #define MAX_LINE_SIZE 8
 
+int g_problems2dampen = 0;
+
 void resetArray(int arr[]) { 
     for (int i = 0; i < MAX_LINE_SIZE; i++) { 
         arr[i] = 0;
     }
 }
 
-int isSafe(int nums[], int array_size, int recurse) { 
+int isSafe(int nums[], int array_size) { 
     int increase = 0;
     int safe = 1;
     int delta;
-    int problems2dampen = 0;
-    int problem_node = 0; // store index of last unsafe node
 
-    printf("*%d* ", nums[0]);
     // If all levels are (all increase or all decrease) && (any two adjacent levels differ by 1 <= x <= 3) == safe
     for (int i = 1; i < array_size && nums[i]; i++) { 
-        delta = nums[i] - nums[i-1];
-        if (i == 1) { 
-            // Set our pattern
-            increase = delta; // will be negative for decrease and pos for increase
-        }
-
-        if (nums[i] == 0) { // end of array
+        if (nums[i] == 0)// end of array
             break;
-        }        
+
+        delta = nums[i] - nums[i-1];
+
+        if (i == 1)
+            increase = delta; // will be negative for decrease and pos for increase
 
         // Was the delta in the valid range?
-        if (delta*increase > 0 && ((abs(delta) >= 1 && abs(delta) <= 3))) { 
-            printf("[%d] ", nums[i]);
-        } else { 
-            printf("!%d! ", nums[i]);
-            problem_node = i;
-            problems2dampen++;
+        if ( !(delta*increase > 0 && ((abs(delta) >= 1 && abs(delta) <= 3))) ) { 
+            g_problems2dampen++;
             safe = 0;
         }
     }
 
-    // Check if removing the problem node would help
-    printf("#%d-%d# ", problems2dampen, recurse);
+    return safe;
+}
 
-    if (problems2dampen == 1 && recurse == 1) {
-        // Check if removing the problem node would help 
-        printf("\nREFACTOR? %d -- > ", problem_node);
-        int clone_arr[MAX_LINE_SIZE-1]= {0};
-        int idx_delta = 0;
-        for (int i = 0; i < array_size && idx_delta < array_size-1; i++) {
-            if (nums[i] == 0) { // end of array
-                break;
-            } else if (i == problem_node) { 
-                continue;
-            }
+int remove_node_check(int arr[], int node) {
+    int new_arr_idx = 0;
+    int new_arr[MAX_LINE_SIZE-1] = {0};
 
-            clone_arr[idx_delta] = nums[i];
-            printf("%d ", clone_arr[idx_delta]);
-            idx_delta++;
-        }
-        printf("\n");
-        // Try again? 
-        if (isSafe(clone_arr, array_size-1, 0)) { 
-            return 1;
-        }
-        return 0;
+    for (int i = 0; i < MAX_LINE_SIZE; i++) { 
+        if (i == node)
+            continue;
+
+        if (arr[i] == 0)
+            break;
+
+        // otherwise continue copying
+        new_arr[new_arr_idx] = arr[i];
+        new_arr_idx++;
     }
 
+    return isSafe(new_arr, MAX_LINE_SIZE-1);
+}
+
+int isitActuallysafe(int nums[], int array_size) { 
+    int safe = 0;
+
+    if (isSafe(nums, array_size))
+        safe = 1;
+    else if (g_problems2dampen) {
+        // Remove every node to see if a possible solution exists
+        for (int i = 0; i < MAX_LINE_SIZE && !safe; i++) { 
+            safe = remove_node_check(nums, i);
+        }
+    } else
+        safe = 0;
+
+    g_problems2dampen = 0;
     return safe;
 }
 
@@ -79,6 +81,7 @@ int main() {
     char *token; // tokenizer for space delimeted numbers
     int idx = 0; // Index for tokenizer
     int num_safe = 0; // Number of reports that are safes
+    int num_safe_act = 0; // Number of reports safe with dampener
 
     // Open input file 
     f = fopen("input.txt", "r"); 
@@ -99,17 +102,18 @@ int main() {
         }
 
         // Determine if safe
-        if (isSafe(numbers,MAX_LINE_SIZE, 1)) {
-            printf("Safe!\n");
+        if (isSafe(numbers,MAX_LINE_SIZE))
             num_safe++;
-        } else { 
-            printf("Unsafe!\n");
-        }
+        
+        if (isitActuallysafe(numbers,MAX_LINE_SIZE))
+            num_safe_act++;
 
         resetArray(numbers); // reset array to 0 
     }
 
     printf("%d reports are safe!\n", num_safe);
+    printf("%d reports are safe with problem dampener!\n", num_safe_act);
+
 
     return 0;
 }
