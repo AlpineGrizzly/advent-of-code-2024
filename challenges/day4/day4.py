e3ac7bb6c614d6e5abbe1abdabea49bf8f998b8d
r3v <yada@yada.com>
2024-12-22 02:35:51 -0500

    small stuff

--- challenges/day4/day4.py
+++ challenges/day4/day4.py
@@ -8,23 +8,19 @@ def get_xmas_count(array):
     # Iterate through array by increments of len(word2find) to ensure repeats arent counted
     for x in range(0, len(array[0])-len(word2find)+1):
         for y in range(0, len(array)-len(word2find)+1):
-            #debug_arr = [len(word2find)][len(word2find)] 
-
-            # Check all rows both forward and reversed
             for m in range(0, len(word2find)): 
                 # Get row and column values 
                 row = array[x+m][y:y+len(word2find)]
                 col = array[x][y+m] + array[x+1][y+m] + array[x+2][y+m] + array[x+3][y+m]
                 
-                # Check all directions forward and reverse 
+                # Check row and col forward and reversed
                 if row == word2find or row[::-1] == word2find:
                     print("Found row ", row)
                     count += 1
                 if col == word2find or col[::-1] == word2find:
                     print("Found col ", col)
                     count += 1
-
-            
+ 
             # Check all both verticals 
             # Get vertical
             left_vert, right_vert = "", ""
@@ -33,10 +29,10 @@ def get_xmas_count(array):
                 right_vert += array[x+len(word2find)-1-n][y+n]
            
             if left_vert == word2find or left_vert[::-1] == word2find:
-                print("Found lv rv", left_vert)
+                print("Found lv ", left_vert)
                 count += 1
             if right_vert == word2find or right_vert[::-1] == word2find:
-                print("Found rv", right_vert)
+                print("Found rv ", right_vert)
                 count += 1
 
             print("-----------REAL ---------------")
@@ -53,7 +49,7 @@ def main():
     search_box = []
  
     # Get number of lines in the input file and create two arrays of that sizes
-    with open("test_input.txt", "r") as file:
+    with open("small_test_input.txt", "r") as file:
         for line in file:
             search_box.append(line.rstrip('\n'))
 
